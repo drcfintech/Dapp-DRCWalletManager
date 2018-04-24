82b6cef9a30b777b88470fe52a04e1a34d8e65d6
Zhi Zhang <zhangzhi@rcitech.cn>
2018-04-24 22:18:05 +0800

    modify gas limit

--- DRCWalletManager/migrations/2_deploy_contracts.js
+++ DRCWalletManager/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var DRCWalletMgrCon = artifacts.require("./DRCWalletManager.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DRCWalletMgrCon, {gas: '5775218', gasPrice: '4000000000'})
+  deployer.deploy(DRCWalletMgrCon, {gas: '6975218', gasPrice: '2000000000'})
   .then(function(instance) {
     console.log(instance);
   });
