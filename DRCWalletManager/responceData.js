9d518f2564a2f3b7faa6a403bb43fdc0378b90a6
Zhi Zhang <zhangzhi@rcitech.cn>
2018-04-25 17:19:09 +0800

    upload other modified files

--- DRCWalletManager/responceData.js
+++ DRCWalletManager/responceData.js
@@ -1,27 +1,39 @@
 module.exports = {
-    hashAlreadyInserted: {
+    depositAlreadyExist: {
         status: "failed",
-        msg: "输入了重复的Hash值"
+        msg: "输入的钱包地址已经有对应的充值地址"
     },
     lowBalance: {
         status: "failed",
         msg: "上链账号余额不足"
     },
+    notEnoughBalance: {
+        status: "failed",
+        msg: "提现余额不足"
+    },
     evmError: {
         status: "failed",
         msg: "其他"
     },
-    selectHashSuccess: {
+    getDepositAddrSuccess: {
         status: "success",
-        msg: "查询成功"
+        msg: "获取充值地址成功"
+    },
+    withdrawSuccess: {
+        status: "success",
+        msg: "提现成功"
+    },
+    withdrawFailed: {
+        status: "failed",
+        msg: "提现失败"
     },
     selectHashFailed: {
         status: "failed",
-        msg: "查询失败"
+        msg: "获取充值地址失败"
     },
-    insertHashSuccess: {
+    createDepositAddrSuccess: {
         status: "success",
-        msg: "上链成功"
+        msg: "创建充值地址成功"
     },
     validationFailed: {
         status: "failed",
