fbecdba1024a9c977158b4eeb70635aecf9ac213
Zhi Zhang <zhangzhi@rcitech.cn>
2018-05-28 20:10:17 +0800

    fix error in callback function

--- DRCWalletManager/server.js
+++ DRCWalletManager/server.js
@@ -731,13 +731,15 @@ var Actions = {
           // let withdrawAddrNameBytes = web3.eth.abi.encodeParameter('bytes32', withdrawAddrName);
           let withdrawAddrNameBytes = '0x' + web3Coder.encodeParam('bytes32', withdrawAddrName);  
           console.log(withdrawAddrNameBytes);
+          var realValue = web3.utils.toBN(requestObject.value).mul(1e18);
+          console.log("real withdraw value is ", realValue);
           let encodeData_params = web3.eth.abi.encodeParameters(
             ['address', 'uint256', 'bytes32', 'address','uint256','bool'], 
             [requestObject.depositAddress.slice(2), 
               depositTime, 
               withdrawAddrNameBytes,
               requestObject.withdrawAddress.slice(2),
-              requestObject.value * 1e18,
+              realValue,
               false]
           );
           console.log(encodeData_params);
