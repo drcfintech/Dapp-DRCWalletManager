2382215b5f72dcf75b6db4e5f3487fbff75440e4
Zhi Zhang <zhangzhi@rcitech.cn>
2018-11-15 12:15:19 +0800

    modify file structures

--- TruffleFrontEndDemo/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Credit = artifacts.require("./Credit.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Credit);
-};
--- TruffleNodeServer_V4/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var HashDataCon = artifacts.require("./HashDataCon.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(HashDataCon);
-};
